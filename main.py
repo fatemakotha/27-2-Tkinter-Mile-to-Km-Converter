afda712e695d989de7a0ec82efe5a6af332c4e5f
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-21 21:52:00 +0600

    Created window

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
 from tkinter import *
+
+my_window = Tk()
+my_window.title("My first GUI program")
+my_window.minsize(width=500, height=300)
+#padding the my_window:
+my_window.config(padx=200, pady=100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+my_window.mainloop()
