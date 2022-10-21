2099ca10a8f1227d8f0894599377343c3bf534f5
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-21 22:10:11 +0600

    Fundamentals up and running

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
 from tkinter import *
 
 my_window = Tk()
-my_window.title("My first GUI program")
+my_window.title("Miles to Km Converter")
 my_window.minsize(width=500, height=300)
 #padding the my_window:
-my_window.config(padx=200, pady=100)
+my_window.config(padx=20, pady=20)
+
+def convert():
+    print("hi")
+
+def button_clicked():
+    pass
 
 
 
+#Label: "is equal to"
+my_label = Label(text="is equal to", font=("Arial", 16, ""))
+my_label.grid(column=0, row=1)
 
+#Entry
+input = Entry(width=10)
+print(input.get())
+input.grid(column=1, row=0)
+input.config(width=7)
 
+#Label: gets output from a function:
+converted_value = Label(text="", font=("Arial", 16, ""))
+converted_value.grid(column=1, row=1)
+converted_value.config(text="0")
 
+#Label: "Miles"
+my_label = Label(text="Miles", font=("Arial", 16, ""))
+my_label.grid(column=2, row=0)
 
+#Button: "Calculate"
+my_button = Button(text="Calculate", command=button_clicked)
+my_button.grid(column=1, row=2)
 
-
+#Label: "Km"
+my_calculate_label = Label(text="Km", font=("Arial", 16, ""))
+my_calculate_label.grid(column=2, row=1)
 
 
 
